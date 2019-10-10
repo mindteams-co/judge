e634043c08afe5c750d54e2aea87e246b7b61cdf
Marcin Josiński <marcin.josinski@skygate.io>
2019-10-10 03:13:46 +0200

    update routing login form comp

--- frontend/src/routes/AuthRoute.js
+++ frontend/src/routes/AuthRoute.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AuthService } from '../services';
 
 const AuthRoute = ({ path, component }) => {
-    const isAuthenticated = false;
-    if (!isAuthenticated) {
+    if (!AuthService.currentUserValue) {
         return <Redirect to="/login" />;
     }
+
     return <Route exact path={path} component={component} />;
 };
 
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -3,7 +3,10 @@ import { competitionServiceFactory } from './competition';
 import { StorageService } from '../common/services/storageService.js';
 import { HttpServiceFactory } from './http';
 import { AuthServiceFactory } from './auth';
+import { RoutingService } from './routing';
+import { history } from '../config/history.js';
 
 export const competitionService = competitionServiceFactory(apiBase, fetch);
 export const HttpService = new HttpServiceFactory(apiBase, fetch, StorageService);
-export const AuthService = new AuthServiceFactory(apiBase, HttpService);
+export const AuthService = new AuthServiceFactory(apiBase, HttpService, StorageService);
+export const routingService = new RoutingService(history);
