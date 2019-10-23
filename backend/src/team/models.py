97a5dd5838b36c0bb501f85d907c1d8bfdcfba31
Marcin Josiński <marcin.josinski@skygate.io>
2019-10-23 12:56:11 +0200

    add django admin

--- backend/src/team/models.py
+++ backend/src/team/models.py
@@ -10,11 +10,24 @@ class TeamManager(BaseUserManager):
 
         return team
 
+    def create_superuser(self, email, name, password):
+        team = self.create_team(
+            email,
+            name=name,
+            password=password
+        )
+        team.is_admin = True
+        team.save(using=self._db)
+        return team
+
 
 class Team(AbstractBaseUser):
     email = models.EmailField(unique=True, db_index=True)
     name = models.CharField(max_length=32, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["name"]
@@ -23,3 +36,13 @@ class Team(AbstractBaseUser):
 
     def __str__(self):
         return self.name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
