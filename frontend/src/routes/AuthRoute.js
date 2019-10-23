f2a10d5c0b09e819d32db8d2f50e0445177ef80b
Marcin Josiński <marcin.josinski@skygate.io>
2019-10-23 15:00:21 +0200

    add page for judges

--- frontend/src/routes/AuthRoute.js
+++ frontend/src/routes/AuthRoute.js
@@ -3,12 +3,17 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { authService } from '../services';
 
-const AuthRoute = ({ path, component }) =>
-    !authService.currentUserValue() ? (
-        <Redirect to="/login" />
-    ) : (
-        <Route exact path={path} component={component} />
-    );
+const AuthRoute = ({ path, component }) => {
+    const user = authService.currentUserValue();
+
+    if (!user) {
+        return <Redirect to="/login" />;
+    }
+    if (user && user.user.isAdmin) {
+        return <Redirect to="/" />;
+    }
+    return <Route exact path={path} component={component} />;
+};
 
 AuthRoute.propTypes = {
     path: PropTypes.string.isRequired,
